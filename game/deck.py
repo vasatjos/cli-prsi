7101d4cde68db72c511400f28a44d234f73cc1b1
Josef Vasata <vasatjos@fit.cvut.cz>
2025-07-03 13:59:08 +0200

    Pull out deck class into separate file

--- game/deck.py
+++ game/deck.py
@@ -1,34 +1,6 @@
-from random import shuffle
 from card_utils import Suit, Rank, CardEffect
-from functools import total_ordering
-
-
-@total_ordering
-class Card:
-    def __init__(self, suit: Suit, rank: Rank) -> None:
-        self.suit = suit
-        self.rank = rank
-
-        self.effect: CardEffect | None
-        self._init_effect(rank)
-
-    def _init_effect(self, rank) -> None:
-        if rank is Rank.SEVEN:
-            self.effect = CardEffect.DRAW_TWO
-        elif rank is Rank.OBER:
-            self.effect = CardEffect.CHANGE_SUIT
-        elif rank is Rank.ACE:
-            self.effect = CardEffect.SKIP_TURN
-        else:
-            self.effect = None
-
-    def __repr__(self):
-        return f"{self.rank} of {self.suit}"
-
-    def __lt__(self, other) -> bool:
-        return (
-            self.rank < other.rank or self.rank == other.rank and self.suit < other.suit
-        )
+from card import Card
+from random import shuffle
 
 
 class Deck:
