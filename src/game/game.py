a673c89ff3e4adda8feb76ddfaa7646d4cd5d361
Josef Vasata <vasatjos@fit.cvut.cz>
2025-07-08 16:41:09 +0200

    Implement menu printing and dealing

--- src/game/game.py
+++ src/game/game.py
@@ -1,6 +1,9 @@
+import os
+
 from game.deck import Deck
 from game.player import Player
 from game.state_manager import GameStateManager
+
 
 class Prsi:
     STARTING_HAND = 4
@@ -11,3 +14,45 @@ class Prsi:
         self._players: list[Player] = []
         self._deck: Deck = Deck()
         self._effect_manager: GameStateManager = GameStateManager()
+
+    @staticmethod
+    def _print_menu() -> int:
+        """
+        Print the menu.
+
+        Returns:
+          The number of players who will be playing. 0 is returned if the user
+          wishes to exit.
+        """
+        while True:
+            user_input = input(
+                "Please select the number of players (2-6). Enter 0 to exit: "
+            )
+            try:
+                num_players = int(user_input)
+                if num_players > Prsi.MAX_PLAYER_COUNT:
+                    raise ValueError
+                if num_players < 2:
+                    return 0
+                return num_players
+            except ValueError:
+                os.system("clear")
+                print("Please insert a valid number of players.")
+
+    def _deal(self) -> None:
+        for _ in range(Prsi.STARTING_HAND):
+            for player in self._players:
+                player.take_drawn_cards([self._deck.draw_card()])
+
+    def start_game(self) -> None:
+        self._player_count = self._print_menu()
+        if not self._player_count:
+            return
+        self._players = [Player() for _ in range(self._player_count)]
+        self._deal()
+
+        self._game_loop()
+
+    def _game_loop(self) -> None:
+        while True:
+            pass
